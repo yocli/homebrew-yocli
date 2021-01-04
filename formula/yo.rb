b271b3150c35005666e7379a13e14f29b17f0ff7
Carl Dong <contact@carldong.me>
2021-01-03 19:21:48 -0500

    Bump to v0.1.5

--- formula/yo.rb
+++ formula/yo.rb
@@ -1,8 +1,8 @@
 class Yo < Formula
   desc "CLI to pair with the yo iOS app"
   homepage "https://github.com/yocli/yocli"
-  url "https://github.com/yocli/yocli/archive/v0.1.4.tar.gz"
-  sha256 "7b378bf4b1546781f3c173376a191f5b62765544eeaa9855f7ca50b9830d393d"
+  url "https://github.com/yocli/yocli/archive/v0.1.5.tar.gz"
+  sha256 "6bf9b8dbf5b1438f785f9316770f77d42db9330eccd51d0d42f17cd91bf21337"
   head "https://github.com/yocli/yocli.git"
 
   depends_on "qrencode"
